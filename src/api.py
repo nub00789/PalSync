daace874507dcebe58b473c5ade79c9b0b2eaa68
nub00789 <yusufbadar10@gmail.com>
2026-08-07 03:47:19 +0530

    Add embedded status API

---
+++ src/api.py
@@ -0,0 +1,74 @@
+import json
+import threading
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+
+_state = {
+    "name": "",
+    "hosting": False,
+    "running": False,
+    "sync": 100,
+}
+
+
+def update_state(**kwargs):
+    _state.update(kwargs)
+
+
+def get_state():
+    return dict(_state)
+
+
+class StatusHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        if self.path != "/status":
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        body = json.dumps(get_state()).encode("utf-8")
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-Length", str(len(body)))
+        self.end_headers()
+
+        self.wfile.write(body)
+
+    def log_message(self, *args):
+        return
+
+
+_server = None
+_thread = None
+
+
+def start_api(host="0.0.0.0", port=45678):
+
+    global _server, _thread
+
+    if _server is not None:
+        return
+
+    _server = ThreadingHTTPServer((host, port), StatusHandler)
+
+    _thread = threading.Thread(
+        target=_server.serve_forever,
+        daemon=True,
+        name="PalSyncAPI"
+    )
+
+    _thread.start()
+
+
+def stop_api():
+
+    global _server
+
+    if _server is None:
+        return
+
+    _server.shutdown()
+    _server.server_close()
+    _server = None
